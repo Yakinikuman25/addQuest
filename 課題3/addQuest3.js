f51c76c3f29d6a1a5d1cba0b96b9ee98ceb71c90
Ryo <ryoryokozawa@gmail.com>
2026-03-13 06:02:23 +0900

    Add files via upload

--- 課題3/addQuest3.js
+++ 課題3/addQuest3.js
@@ -1,38 +1,45 @@
-////////////全部の処理を書いた場合
-
+// オブジェクトを作成し、行き先は、配列も設定した
 // 一つ目のコース
-const course1 = { plan: "Aプラン", goal: "北海道", cost: 150000, period: "2泊3日", concept: "食べたいものが多すぎる!!" };
+const course1 = { plan: "Aプラン", goal: ["北海道"], cost: 150000, period: "2泊3日", concept: "食べたいものが多すぎる!!" };
 // 二つ目のコース
-const course2 = { plan: "Bプラン", goal: "京都、奈良", cost: 100000, period: "3泊4日", concept: "和を感じよう。" };
+const course2 = { plan: "Bプラン", goal: ["京都", "奈良"], cost: 100000, period: "3泊4日", concept: "和を感じよう。" };
 // 三つ目のコース
-const course3 = { plan: "Cプラン", goal: "福岡、佐賀、長崎、熊本", cost: 250000, period: "6泊7日", concept: "二日酔い待ったなし" };
+const course3 = { plan: "Cプラン", goal: ["福岡", "佐賀", "長崎", "熊本"], cost: 250000, period: "6泊7日", concept: "二日酔い待ったなし" };
+
+// オブジェクトを配列にし、for文で引き出せるように配列にした。
+const plans = [course1, course2, course3];
+
+console.log("-------------------------");
+
+// 全体の処理を行う
+for(let i = 0; i < plans.length; i++){
+
+    // 各プランについて説明します。を表示する
+    console.log( plans[i].plan + "について説明します。");
+
+    // 行き先の処理、「と」が場合によってあるため、for文とif文を作成
+    let totalGoal = plans[i].goal;
+    //空を追加していくのと、空に行き先が入っていくようにlet addGoalを追加 
+    let addGoal = "";
+    // 処理を行う
+    for (let j = 0; j < totalGoal.length; j++){
+        addGoal = addGoal + totalGoal[j];
+
+        // totalgoalが最後ではなければ「と」語尾につく。
+        if(j != totalGoal.length -1){
+            addGoal = addGoal + "と";
+        }
+
+    };
+    // 行き先を表示する
+    console.log("行き先は、" + addGoal + "です。");
+
+    // 金額と期間を表示する
+    console.log("金額は" + plans[i].cost + "円で、期間は" + plans[i].period + "です。");
+    // コンセプトを表示する
+    console.log("コンセプトは「" + plans[i].concept + "」");
+
+    console.log("-------------------------");
+}
 
 
-// 一つ目の表示
-console.log(course1.plan + "について説明します。");
-console.log("行き先は、" + course1.goal + "です。");
-console.log("金額は" + course1.cost + "円で、期間は" + course1.period + "です。");
-console.log("コンセプトは「" + course1.concept + "」");
-
-// 二つ目の表示
-console.log(course2.plan + "について説明します。");
-console.log("行き先は、" + course2.goal + "です。");
-console.log("金額は" + course2.cost + "円で、期間は" + course2.period + "です。");
-console.log("コンセプトは「" + course2.concept + "」");
-
-// 三つ目の表示
-console.log(course3.plan + "について説明します。");
-console.log("行き先は、" + course3.goal + "です。");
-console.log("金額は" + course3.cost + "円で、期間は" + course3.period + "です。");
-console.log("コンセプトは「" + course3.concept + "」");
-
-
-
-
-////////////まとめて処理を書いた場合
-// 一つ目のプラン
-const course = { plan: plan[i], goal: goal[i], cost: cost[i], period: period[i], concept: [i] };
-
-course.plan[i]= "Aプラン", "Bプラン", "Cプラン";
-
-console.log(course.plan + "について説明します。");
