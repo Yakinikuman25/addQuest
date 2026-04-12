2a884def5cb1b5c8dc1edfbe1c98aecb329b3f6d
Ryo <ryoryokozawa@gamil.com>
2026-04-13 06:04:06 +0900

    課題5の表示の所まで

--- HTML.CSS.JS/kadai_05/addQuest5.js
+++ HTML.CSS.JS/kadai_05/addQuest5.js
@@ -50,22 +50,23 @@ userBtn.addEventListener('click', () => {
     const userGoal = document.getElementById("user-goal");
 
     //HTMLの参加人数ドロップダウンのidを取得し定数に代入する
-    const userEntrant = document.getElementById("user-entrant");
+    const userEntrant = document.getElementById("user-entrant").value;
 
     //HTMLの宿泊数ラジオボタンのidを取得し定数に代入する
     // const userStay = document.getElementById("user-stay");
     const userStay = document.forms.userStay.stay.value;
 
     //HTMLのテキストのidを取得し定数に代入する
-    const userText = document.getElementById("user-text");
+    const userText = document.getElementById("user-text").value;
 
     // HTMLの出力結果を表示するidを取得し定数に代入する
     const output = document.getElementById("output");
 
     
     // 確認用
+    console.log("--------------------------------");
     console.log(userGoal.value);
-    console.log(userEntrant.value);
+    console.log(userEntrant);
     // console.log(userStay.value);
     console.log(userStay);
     console.log(userText);
@@ -84,30 +85,76 @@ userBtn.addEventListener('click', () => {
     // selectGoalにHTMLで選択したuserGoalとオブジェクトのareaの値を代入する
     const selectGoal = selectPlan.area;
     // 確認用
-    console.log(selectGoal);
+    console.log("目的地" + selectGoal);
 
      // selectStayにHTMLで選択したuserStayを「-1」したものとオブジェクトのperiodsのインデックスの同じ値を代入する
     // 例：1泊を選択→　HTML：value=1、js：periods[0]が等しくしたいのでHTML：value=1を-1にして0にする。そうすると0=0で同じ値になる。
     const selectStay = selectPlan.periods[userStay -1];
     // 確認用
-    console.log(selectStay);
+    console.log("宿泊数" + selectStay);
 
     // selectStayCostにHTMLで選択したuserStayを「-1」したものとオブジェクトのcostのインデックスの同じ値を代入する
     // 例：1泊を選択→　HTML：value=1、js：cost[0]が等しくしたいのでHTML：value=1を-1にして0にする。そうすると0=0で同じ値になる。
     const selectStayCost = selectPlan.cost[userStay -1];
     // 確認用
-    console.log(selectStayCost);
+    console.log("宿泊費" + selectStayCost);
 
-    // 費用総額の計算
-    
+    // 交通費
+    for(let j = 0; j < plans.length; j++){
+        // 北海道の場合（人数に関わらず、1泊あたり15,000円）
+        if(selectGoal === "北海道"){
+            carfare = userStay * 15000;
+        }
 
+        // 東京の場合（1人、1泊あたり4,800円）
+        else if(selectGoal === "東京"){
+            carfare = userStay * userEntrant * 4800;
+        }
 
+        // 大阪の場合（1人、1泊あたり4,600円）
+        else if(selectGoal === "大阪"){
+            carfare = userStay * userEntrant * 4600;
+        }
+
+        // 福岡の場合（1人、1泊あたり3,600円）
+        else if(selectGoal === "福岡"){
+            carfare = userStay * userEntrant * 3600;
+        }
+
+        // 沖縄の場合（1~5人、1泊あたり10,000円）
+        else if(selectGoal === "沖縄" && userEntrant <= 5 ){
+            carfare = userStay * 10000;
+        }
+        // 沖縄の場合（6~10人、1泊あたり20,000円）
+        else if(selectGoal === "沖縄" && 5< userEntrant <= 9){
+            carfare = userStay * 20000;
+        }
+    };
+    // 確認用
+    console.log("交通費" + carfare);
+
+    // // 費用総額の計算(消費税0.8%)
+    const totalCost = Math.floor((selectStayCost + carfare) *1.08);
+    console.log("総額" + totalCost);
+
+    // 一人あたりの費用
+    const total = Math.floor(totalCost / userEntrant);
+    console.log("一人当たりの費用" + total);
 
 
     // 出力するものをHTMLに表示する
-    output.innerHTML = "";
+    output.innerHTML = 
+    selectGoal + "行き" + selectStay + "の旅行プランを表示します。" 
+    +  "費用総額は、税込" + totalCost + "円です。"
+    + "1人当たりでは、税込" + total + "円になります。"
+    + "幹事からのメッセージをいただいております。"
+    + "「" + userText + "」"
+    + "楽しんでください！";
+
+    console.log("--------------------------------");
 
 });
+
 
 
 
